6ddf38d3c0dc0e4669d312304a6f9ff46e860a62
Reshma Raj <reshma.raj@speehive.com>
2023-08-08 14:28:17 +0530

    Add project files.

---
+++ FormFunctionApp/Data/GeoFormsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using FormFunctionApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormFunctionApp.Data
+{
+    public class GeoFormsDbContext : DbContext
+    {
+        public GeoFormsDbContext(DbContextOptions<GeoFormsDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Add DbSet properties for your models representing tables in the GeoForms database
+        // For example:
+        // public DbSet<GeoForm> GeoForms { get; set; }
+         public DbSet<SubmittedForm> AppSubmittedForms { get; set; }
+        // ...
+    }
+}
---
+++ FormFunctionApp/Models/SubmittedForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormFunctionApp.Models
+{
+public class SubmittedForm
+    {
+        public Guid Id { get; set; }
+        public Guid VesselId { get; set; }
+ //       public Vessel Vessel { get; set; }
+        public string IMO { get; set; }
+        public string Data { get; set; }
+
+        public string VoyageNo { get; set; }
+
+        public string Port { get; set; }
+
+        public DateTimeOffset Effective { get; set; }
+
+        public DateTimeOffset Recieved { get; set; }
+
+        public string Location { get; set; }
+     //   public SubmittedFormType Type { get; set; }
+      //  public AppSubmittedFormstatus Status { get; set; }
+        public string Remark { get; set; }
+
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string FormIdentifier { get; set; }
+    }
+}
